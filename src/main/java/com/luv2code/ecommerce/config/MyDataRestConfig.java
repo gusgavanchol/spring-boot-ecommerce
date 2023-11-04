6edc853a77a44179c1b4c6e3dea81726de606ddc
gustavo.gavancho_dev <gustavo.gavancho@devsu.com>
2023-11-04 11:34:06 -0500

    Changes

--- src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
@@ -7,6 +7,7 @@ import com.luv2code.ecommerce.entity.State;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.metamodel.EntityType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.core.mapping.ExposureConfigurer;
@@ -21,6 +22,9 @@ import java.util.Set;
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
 
+    @Value("${allowed.origins}")
+    private String[] theAllowedOrigins;
+
     private EntityManager entityManager;
 
     @Autowired
@@ -33,29 +37,19 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 
         HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH};
 
-        // disable HTTP methods for Product: PUT, POST, DELETE and PATCH
-        disableHttpMethod(config.getExposureConfiguration()
-                .forDomainType(Product.class), theUnsupportedActions);
-
-        // disable HTTP methods for ProductCategory: PUT, POST, DELETE and PATCH
-        disableHttpMethod(config.getExposureConfiguration()
-                .forDomainType(ProductCategory.class), theUnsupportedActions);
-
-        // disable HTTP methods for ProductCategory: PUT, POST, DELETE and PATCH
-        disableHttpMethod(config.getExposureConfiguration()
-                .forDomainType(Country.class), theUnsupportedActions);
-
-        // disable HTTP methods for ProductCategory: PUT, POST, DELETE and PATCH
-        disableHttpMethod(config.getExposureConfiguration()
-                .forDomainType(State.class), theUnsupportedActions);
+        disableHttpMethod(config.getExposureConfiguration().forDomainType(Product.class), theUnsupportedActions);
+        disableHttpMethod(config.getExposureConfiguration().forDomainType(ProductCategory.class), theUnsupportedActions);
+        disableHttpMethod(config.getExposureConfiguration().forDomainType(Country.class), theUnsupportedActions);
+        disableHttpMethod(config.getExposureConfiguration().forDomainType(State.class), theUnsupportedActions);
 
         exposeIds(config);
+
+        cors.addMapping(config.getBasePath() + "/**").allowedOrigins(theAllowedOrigins);
     }
 
     private static void disableHttpMethod(ExposureConfigurer config, HttpMethod[] theUnsupportedActions) {
-        config
-                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+        config.withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+              .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
     }
 
     private void exposeIds(RepositoryRestConfiguration config){
